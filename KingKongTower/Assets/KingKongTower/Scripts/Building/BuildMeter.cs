419217c885de96a80dbbdf2267320d01116bb75e
AniSkyWorker <holyrecycler@mail.ru>
2017-12-21 01:45:57 +0300

    релизовал интерфей IPickerObserver в BuildMeter

--- KingKongTower/Assets/KingKongTower/Scripts/Building/BuildMeter.cs
+++ KingKongTower/Assets/KingKongTower/Scripts/Building/BuildMeter.cs
@@ -1,30 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Scripts.Building.Pickers;
 using UnityEngine;
- 
-public interface IPositionPercent
-{
-    float PositionPercent { set; }
-}
 
- public class BuildMeter : MonoBehaviour, IPositionPercent
+namespace Scripts.Building
 {
-    public float maxHeight;    //the max height at which we will change direction
-    public float minHeight;    //the min height at which we will change direction
-
-    public float PositionPercent
+    public class BuildMeter : MonoBehaviour, IPickerObserver
     {
-        set
+        public float maxHeight;    //the max height at which we will change direction
+        public float minHeight;    //the min height at which we will change direction
+        private IPickerSubject _pickSubject;
+
+        void Start()
         {
-            float normalizedPositionPercentRatio = (value + 100) / 200;
-            transform.position = new Vector3(minHeight + (maxHeight - minHeight) * normalizedPositionPercentRatio, transform.position.y,  transform.position.z );
+            _pickSubject = FindObjectOfType<PickerSubject>();
+            Register();
+        }
+
+        public void Register()
+        {
+            _pickSubject.AddListener(this);
+        }
+
+        public void SetPosition(float position)
+        {
+            float normalizedPositionPercentRatio = (position + 100) / 200;
+            transform.position = new Vector3(minHeight + (maxHeight - minHeight) * normalizedPositionPercentRatio, transform.position.y, transform.position.z);
+        }
+
+        public void Stop()
+        {
+            enabled = false;
+        }
+
+        public void Unregister()
+        {
+            _pickSubject.RemoveListener(this);
         }
     }
-
-    void Update()
-    {
-        PositionPercent = 100;
-    }
-
 }
 
