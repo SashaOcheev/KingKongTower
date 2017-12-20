88ef9ebc752f88afae007b5161d8f227d5f743a8
AniSkyWorker <holyrecycler@mail.ru>
2017-12-20 23:30:36 +0300

    добавил пикер, для отображения корректности позиции блока

---
+++ KingKongTower/Assets/KingKongTower/Scripts/Building/BuildMeter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public interface IPositionPercent
+{
+    float PositionPercent { set; }
+}
+
+ public class BuildMeter : MonoBehaviour, IPositionPercent
+{
+    public float maxHeight;    //the max height at which we will change direction
+    public float minHeight;    //the min height at which we will change direction
+
+    public float PositionPercent
+    {
+        set
+        {
+            float normalizedPositionPercentRatio = (value + 100) / 200;
+            transform.position = new Vector3(minHeight + (maxHeight - minHeight) * normalizedPositionPercentRatio, transform.position.y,  transform.position.z );
+        }
+    }
+
+    void Update()
+    {
+        PositionPercent = 100;
+    }
+
+}
+
